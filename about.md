118156b57cdcdb0edf4125e7913b694bc1fbdcd6
Ivanka Todorova <i.todorova@dineout.bg>
2022-01-04 17:17:03 +0200

    feat: mobile & desktop navigation, logo

---
+++ about.md
@@ -0,0 +1,10 @@
+---
+title: about
+layout: page
+---
+
+<div class="flex h-screen">
+  <div class="m-auto">
+    <h1 class="text-green text-h2">This is my about page.</h1>
+  </div>
+</div>
---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+    // Mobile navigation
+    const btn = document.querySelector("button.mobile-menu-button");
+    const menu = document.querySelector(".mobile-menu");
+
+    btn.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+    });
+});
